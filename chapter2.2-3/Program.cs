b545a9a438e151b46712da2ad50ea71583a96b3d
username <1151555861@qq.com>
2021-06-22 13:31:03 +0800

    first commit

---
+++ chapter2.2-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Specialized;
+namespace chapter2._2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNames(GenerateNames());
+            Console.ReadKey();
+        }
+          static StringCollection GenerateNames()
+           {
+            StringCollection names = new StringCollection();
+            names.Add("Gamma");
+            names.Add("Vlissides");
+            names.Add("Johnson");
+            names.Add("Helm");
+            return names;
+           }
+           static void PrintNames(StringCollection names)
+           {
+               foreach (string name in names)
+               {
+                   Console.WriteLine(name);
+               }
+           }
+
+    }
+}
